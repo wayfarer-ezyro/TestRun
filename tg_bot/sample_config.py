d846700d190105f07f6c940f643dba5ec3f8ebce
wayfarer-ezyro <72940489+wayfarer-ezyro@users.noreply.github.com>
2021-05-08 13:29:17 +0530

    Update sample_config.py

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -10,13 +10,13 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = "YOUR KEY HERE"
-    OWNER_ID = "YOUR ID HERE"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "YOUR USERNAME HERE"
+    API_KEY = "1882387496:AAG4tG_inVamsAOqbiaXhMLAc92RI_NNtmQ"
+    OWNER_ID = "1453690249"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "AidanNia"
 
     # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
-    MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
+    SQLALCHEMY_DATABASE_URI = 'postgres://nqfytkniogdnux:e71df9255f09444f19d0fd41dbf79493de7697bb9d8f2a88eedecaaa988cf679@ec2-3-216-129-140.compute-1.amazonaws.com:5432/d5bb7cqvnjbrr'  # needed for any database modules
+    MESSAGE_DUMP = -1001435803865  # needed to make sure 'save from' messages persist
     LOAD = []
     # sed has been disabled after the discovery that certain long-running sed commands maxed out cpu usage
     # and killed the bot. Be careful re-enabling it!
@@ -25,17 +25,17 @@ class Config(object):
     URL = None
 
     # OPTIONAL
-    SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
-    SUPPORT_USERS = []  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    SUDO_USERS = [1453690249]  # List of id's (not usernames) for users which have sudo access to the bot.
+    SUPPORT_USERS = [1453690249]  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    WHITELIST_USERS = [1453690249]  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
     DONATION_LINK = None  # EG, paypal
     CERT_PATH = None
     PORT = 5000
-    DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
-    STRICT_GBAN = False
+    DEL_CMDS = True  # Whether or not you should delete "blue text must click" commands
+    STRICT_GBAN = True
     WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
-    BAN_STICKER = 'CAADAgADOwADPPEcAXkko5EB3YGYAg'  # banhammer marie sticker
-    ALLOW_EXCL = False  # Allow ! commands as well as /
+    BAN_STICKER = ''  # banhammer marie sticker
+    ALLOW_EXCL = True  # Allow ! commands as well as /
 
 
 class Production(Config):
